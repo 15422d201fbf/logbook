8e222018c3d51dd85a6ceb0dc4c30b76b0539d0e
15422d201fbf <urjvah@gmail.com>
2023-05-24 22:50:09 +0200

    started to modify searching logic and functions, modified readme file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, math
 
 #check sudo privileges
 if not os.geteuid() == 0: sys.exit("You need to be root to perform this action.")
@@ -7,19 +7,19 @@ global DATE
 DATE = os.popen("date --iso-8601").read().strip()
 print(DATE)
 global PATH
-PATH = '/home/nathan/logbook/data/'
+PATH = '/home/yourname/logbook/data/'
 
-#check file exists functions
-def exists():
-    return DATE in os.listdir('data')
+def exists(x):
+    rec_date = date.split('-')
+
 
 #prompt main user choices
 def main():
     if exists():
-        print("Logbook already exists. Delete, Modify, Read, Search, Quit (1/2/3/4,5)\n")
+        print("Logbook already exists. Delete, Modify, Read, Quit (1/2/3/4)\n")
         choice = int(input(": "))
 
-        choice_dic = {1: delete, 2: modify, 3: read, 4: search, 5: shutdown}
+        choice_dic = {1: delete, 2: modify, 3: read, 4: shutdown}
         choice_dict = choice_dic.get(choice, error)
         choice_dict()
 
@@ -27,43 +27,44 @@ def main():
         with open(PATH + DATE, 'w') as f:
             content = str(input("\nWrite your logbook > "))
             f.write(content)
+            #store with sort function
+            sort_file_automatically(DATE)
 
         sys.exit("Logbook created.")
 
 
 #create user functions:
 def delete():
+    #modify
     os.system(f"rm -rf {PATH}{DATE}") 
 
 def modify():
+    #modify
     os.system(f"open {PATH}{DATE}") 
 
 def read():
+    #modify
     file =  PATH + DATE
     print(file + "\n")
     os.system(f"cat {PATH}{DATE}")
     print("\n")
 
-def search():
-    print("Group by, search with keyword (1/2)")
-    choice = int(input("\n: "))
 
-    if choice == 1:
-        print("Group by: week, month, year (1/2/3)")
-        sort = int(input("\n: "))
-        if sort == 1:
-            pass
+def sort_file_automatically():
+    #check if year file exists
+    splitted_date = DATE.split("-")
+    year, month, day = splitted_date[0], splitted_date[1], splitted_date[2]
+    week = math.ceil(day / 7)
 
-        elif sort == 2:
-            pass
+    #check if year exists, if not we have to create all the other folders 
+    if not year in os.listdir('data'):
+        os.mkdir(PATH + year)
+        print(f"Created {year} folder in {PATH}")
+        os.mkdir(PATH + year + '/' + month)
+        print(f"Created {month} folder in {PATH}{year}")
+        os.mkdir(PATH + year + '/' + month + '/' + week)
 
-        elif sort == 3:
-            FILES = os.listdir('data')
-            print(FILES)
-
-        else:
-            error()
-
+    
 
 
 def shutdown():
